23705a08977cbb8298b2b0f2fe981db094dcf4a4
Khambar Dussaliyev <dusaliyev@kolesa.kz>
2023-02-28 12:53:21 +0600

    ML-414   Написать базис для библиотеки определюящую подходящую видеокарту
    Форматирование

--- tests/test_gpu_selector.py
+++ tests/test_gpu_selector.py
@@ -1,22 +1,20 @@
-
 import pytest
 from gpu_utils.gpu import GPU
 from gpu_utils.gpu_selector import BestFitGPUSelector
 
 
 class TestBestFitGPUSelector:
-
     def test_select_valid(self):
         gpu_selector = BestFitGPUSelector()
         gpus = [
-            GPU(0, 'Geforce 3060', 12000, 9000),
-            GPU(1, 'Geforce 3060', 12000, 11000),
-            GPU(2, 'Tesla V-100', 32000, 12000),
+            GPU(0, "Geforce 3060", 12000, 9000),
+            GPU(1, "Geforce 3060", 12000, 11000),
+            GPU(2, "Tesla V-100", 32000, 12000),
         ]
         available = gpu_selector.select(gpus, 2000)
         assert available is not None
         assert available.cuda_id == 0
-        assert available.name == 'Geforce 3060'
+        assert available.name == "Geforce 3060"
         assert available.max_available_memory_bytes == 12000
         assert available.current_memory_utlization_bytes == 9000
 
@@ -30,8 +28,8 @@ class TestBestFitGPUSelector:
     def test_select_no_available_gpu(self):
         gpu_selector = BestFitGPUSelector()
         gpus = [
-            GPU(0, 'Geforce 3060', 12000, 9000),
-            GPU(1, 'Geforce 3060', 12000, 11000),
+            GPU(0, "Geforce 3060", 12000, 9000),
+            GPU(1, "Geforce 3060", 12000, 11000),
         ]
         available = gpu_selector.select(gpus, 4000)
         assert available is None
